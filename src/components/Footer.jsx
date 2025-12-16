790d1a1fa2eb9a1aaecdf634742d63eaf9d37934
Dishon John Siddapur <dishon110205@gmail.com>
2025-12-16 20:12:49 +0530

    screen size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -225,7 +225,7 @@ export default function Footer() {
             onChange={(e) => setCountry(e.target.value)}
             className="w-full md:w-48 flex-1 rounded-md border border-white/30 bg-white/20 backdrop-blur-md px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            <option value="">Select your country</option>
+            <option  value="">Select your country</option>
             {countries.map((c) => (
               <option key={c.alpha2} value={c.name}>
                 {c.name}
